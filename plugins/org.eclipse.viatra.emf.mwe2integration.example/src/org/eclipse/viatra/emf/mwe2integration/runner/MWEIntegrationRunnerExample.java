47e393d44858f1a739a768875cd9e329acb476e3
Peter Lunk <lunk.peter@incquerylabs.com>
2015-05-29 16:11:14 +0200

    [470151] Added the revised, second version of the MWE2 VIATRA integration API.
    Change-Id: I8a3795d5388a1d81ffd4f14993b409827a8fea60
    Signed-off-by: Peter Lunk <lunk.peter@incquerylabs.com>

---
+++ plugins/org.eclipse.viatra.emf.mwe2integration.example/src/org/eclipse/viatra/emf/mwe2integration/runner/MWEIntegrationRunnerExample.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2004-2015, Peter Lunk, Zoltan Ujhelyi and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Peter Lunk - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.viatra.emf.mwe2integration.runner;
+
+import java.util.HashMap;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.mwe2.language.Mwe2StandaloneSetup;
+import org.eclipse.emf.mwe2.launch.runtime.Mwe2Runner;
+import org.eclipse.emf.mwe2.runtime.workflow.WorkflowContextImpl;
+import org.junit.Test;
+
+import com.google.inject.Injector;
+
+/**
+ * Test case that demonstrates how an MWE framework should be called programmatically. 
+ * 
+ * Note: Always run as a plain JUnit test.
+ * 
+ * @author Peter Lunk
+ *
+ */
+public class MWEIntegrationRunnerExample {
+    static String location = "src/org/eclipse/viatra/emf/mwe2integration/demo/TransformationDemo.mwe2";
+    
+    @Test
+    public void test() {
+        Injector injector = new Mwe2StandaloneSetup().createInjectorAndDoEMFRegistration();
+        Mwe2Runner mweRunner = injector.getInstance(Mwe2Runner.class);
+        mweRunner.run(URI.createURI(location), new HashMap<String,String>(), new WorkflowContextImpl());
+    }
+}
